07b0a7ac4bc744e3b8255d24f5e9126bdd49c1f0
your_GitHub_tranrobin <robintran@n236-80.mtholyoke.edu>
2023-10-12 21:19:34 -0400

    Fix

--- src/routing/routing.jsx
+++ src/routing/routing.jsx
@@ -1,13 +1,10 @@
 import {Route, Routes, BrowserRouter } from "react-router-dom";
 import LandingPage from "../screen/landing_screen/LandingPage";
-import AboutMe from "/Users/robintran/Documents/qu…/react-port/src/components/landingpage/aboutme/AboutMe.jsx";
-
 function Routing() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<LandingPage />} />
-				<Route aboutme = "aboutme" element={<AboutMe />} />
 			</Routes>
 		</BrowserRouter>
 	);
